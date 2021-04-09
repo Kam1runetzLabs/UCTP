14a6d446366aeb6a8271e07cce93a86cf41c4852
Timofey Konoplyov <timofey.konoplyv@gmail.com>
2021-04-09 19:57:13 +0300

    add sources with Boost link

---
+++ src/application_server/sources/source.cpp
@@ -0,0 +1,7 @@
+#include <boost/thread.hpp>
+#include <boost/regex.h>
+#include <boost/signals2.hpp>
+
+int main() {
+    return 0;
+}
---
+++ src/http_server/sources/source.cpp
@@ -0,0 +1,6 @@
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+
+int main() {
+    return 0;
+}
