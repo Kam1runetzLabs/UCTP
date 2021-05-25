88610573e83d2331a386bc0957a9ba457aeb3dbc
Kam1runetz <notsoserious2017@gmail.com>
2021-05-25 17:40:32 +0300

    CMakeLists.txt fixed

--- models/timetable.cpp
+++ models/timetable.cpp
@@ -4,19 +4,21 @@
 #include <TreeFrogModel>
 
 #include "sqlobjects/timetableobject.h"
+#include "data_for_run.hpp"
+#include "genetic_algorithm.hpp"
 
 Timetable::Timetable() : TAbstractModel(), d(new TimetableObject()) {
-  // set the initial parameters
+    // set the initial parameters
 }
 
 Timetable::Timetable(const Timetable &other) : TAbstractModel(), d(other.d) {}
 
 Timetable::Timetable(const TimetableObject &object)
-    : TAbstractModel(), d(new TimetableObject(object)) {}
+        : TAbstractModel(), d(new TimetableObject(object)) {}
 
 Timetable::~Timetable() {
-  // If the reference count becomes 0,
-  // the shared data object 'TimetableObject' is deleted.
+    // If the reference count becomes 0,
+    // the shared data object 'TimetableObject' is deleted.
 }
 
 int Timetable::id() const { return d->id; }
@@ -24,7 +26,7 @@ int Timetable::id() const { return d->id; }
 int Timetable::classroomId() const { return d->classroomId; }
 
 void Timetable::setClassroomId(int classroomId) {
-  d->classroomId = classroomId;
+    d->classroomId = classroomId;
 }
 
 int Timetable::timeId() const { return d->timeId; }
@@ -36,85 +38,85 @@ int Timetable::blockId() const { return d->blockId; }
 void Timetable::setBlockId(int blockId) { d->blockId = blockId; }
 
 Timetable &Timetable::operator=(const Timetable &other) {
-  d = other.d;  // increments the reference count of the data
-  return *this;
+    d = other.d;  // increments the reference count of the data
+    return *this;
 }
 
 Timetable Timetable::create(int classroomId, int timeId, int blockId) {
-  TimetableObject obj;
-  obj.classroomId = classroomId;
-  obj.timeId = timeId;
-  obj.blockId = blockId;
-  if (!obj.create()) {
-    return Timetable();
-  }
-  return Timetable(obj);
+    TimetableObject obj;
+    obj.classroomId = classroomId;
+    obj.timeId = timeId;
+    obj.blockId = blockId;
+    if (!obj.create()) {
+        return Timetable();
+    }
+    return Timetable(obj);
 }
 
 Timetable Timetable::create(const QVariantMap &values) {
-  Timetable model;
-  model.setProperties(values);
-  if (!model.d->create()) {
-    model.d->clear();
-  }
-  return model;
+    Timetable model;
+    model.setProperties(values);
+    if (!model.d->create()) {
+        model.d->clear();
+    }
+    return model;
 }
 
 QList<TimetableObject> generateTimetable(const QList<Block> &blocks,
                                          const QList<Classroom> &classrooms,
                                          const QList<TimeSlot> &timeSlots) {
-  // todo generate timetable from genetic algorithm
-  return QList<TimetableObject>();
+    // todo generate timetable from genetic algorithm
+    return QList<TimetableObject>();
 }
 
 Timetable::Status Timetable::calculate(const QList<Block> &blocks,
                                        const QList<Classroom> &classrooms,
                                        const QList<TimeSlot> &timeSlots,
                                        QList<Timetable> &result) {
-  // todo arguments validation
-  Status status;
-  /*
-   * if (args invalid) status = error;
-   * return status;
-   * */
+    // todo arguments validation
+    Status status;
+    /*
+     * if (args invalid) status = error;
+     * return status;
+     * */
 
-  auto task = [&blocks, &classrooms, &timeSlots, &result] {
-    auto objects = generateTimetable(blocks, classrooms, timeSlots);
-    for (auto &obj : objects) {
-      if (obj.create()) {
-        result.append(Timetable(obj));
-      }
-    }
-  };
-  QThreadPool::globalInstance()->start(task);
-  return status;
+    auto task = [&blocks, &classrooms, &timeSlots, &result] {
+        auto objects = generateTimetable(blocks, classrooms, timeSlots);
+        for (auto &obj : objects) {
+            if (obj.create()) {
+                result.append(Timetable(obj));
+            }
+        }
+    };
+    QThreadPool::globalInstance()->start(task);
+    return status;
 }
 
 Timetable Timetable::get(int id) {
-  TSqlORMapper<TimetableObject> mapper;
-  return Timetable(mapper.findByPrimaryKey(id));
+    TSqlORMapper<TimetableObject> mapper;
+    return Timetable(mapper.findByPrimaryKey(id));
 }
 
 int Timetable::count() {
-  TSqlORMapper<TimetableObject> mapper;
-  return mapper.findCount();
+    TSqlORMapper<TimetableObject> mapper;
+    return mapper.findCount();
 }
 
 QList<Timetable> Timetable::getAll() {
-  return tfGetModelListByCriteria<Timetable, TimetableObject>(TCriteria());
+    return tfGetModelListByCriteria<Timetable, TimetableObject>(TCriteria());
 }
 
 QJsonArray Timetable::getAllJson() {
-  QJsonArray array;
-  TSqlORMapper<TimetableObject> mapper;
+    QJsonArray array;
+    TSqlORMapper<TimetableObject> mapper;
 
-  if (mapper.find() > 0) {
-    for (TSqlORMapperIterator<TimetableObject> i(mapper); i.hasNext();) {
-      array.append(QJsonValue(
-          QJsonObject::fromVariantMap(Timetable(i.next()).toVariantMap())));
+    if (mapper.find() > 0) {
+        for (TSqlORMapperIterator<TimetableObject> i(mapper); i.hasNext();) {
+            array.append(QJsonValue(
+                    QJsonObject::fromVariantMap(Timetable(i.next()).toVariantMap())));
+        }
     }
-  }
-  return array;
+    return array;
 }
 
 TModelObject *Timetable::modelData() { return d.data(); }
@@ -122,16 +124,16 @@ TModelObject *Timetable::modelData() { return d.data(); }
 const TModelObject *Timetable::modelData() const { return d.data(); }
 
 QDataStream &operator<<(QDataStream &ds, const Timetable &model) {
-  auto varmap = model.toVariantMap();
-  ds << varmap;
-  return ds;
+    auto varmap = model.toVariantMap();
+    ds << varmap;
+    return ds;
 }
 
 QDataStream &operator>>(QDataStream &ds, Timetable &model) {
-  QVariantMap varmap;
-  ds >> varmap;
-  model.setProperties(varmap);
-  return ds;
+    QVariantMap varmap;
+    ds >> varmap;
+    model.setProperties(varmap);
+    return ds;
 }
 
 // Don't remove below this line
